36eac74a46214d71be13087af778adb76b6d4080
Muhammad Qasim <mqas2022@gmail.com>
2022-05-08 18:48:30 +0500

    some changes

---
+++ src/graphs/graphs.rs
@@ -0,0 +1,11 @@
+
+
+struct Node<T> {
+    state: T,
+    actions: Vec
+}
+
+fn name() -> i32 {
+    
+    1
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     println!("Hello, world!");
-    Has
 }
