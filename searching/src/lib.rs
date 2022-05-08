063ea0b192da18e6227220f486a804f0a3803ab2
Muhammad Qasim <mqas2022@gmail.com>
2022-05-08 18:43:48 +0500

    ...

--- searching/src/lib.rs
+++ searching/src/lib.rs
@@ -1,7 +1,17 @@
 #![allow(non_snake_case)]
+#![allow(dead_code)]
 
 mod InformedSeaching;
 mod UninformedSearching;
+
+
+struct Node <T>{
+    state: T,
+    self_cost: u16,
+    heur_cost: u16,
+    nbrs: HashMap
+}
+
 
 pub fn runner() {
     println!("Goodbye, Mars");
